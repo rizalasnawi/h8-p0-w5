b0319948636948f0eee0cfdd124056be2aa7bcf8
rizalasnawi <44157537+rizalasnawi@users.noreply.github.com>
2018-11-18 18:11:29 +0700

    cari pelaku

---
+++ Cari Pelaku (Regex).js
@@ -0,0 +1,11 @@
+function cariPelaku(str){
+    var abc=str.match(/abc/g)
+    var jumlahAbc=abc.length
+    return jumlahAbc
+}
+
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
