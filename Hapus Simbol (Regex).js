8ab2d5e47bb325c85ae6f47b844e090594b9cfb8
rizalasnawi <44157537+rizalasnawi@users.noreply.github.com>
2019-01-30 15:10:43 +0700

    update

--- Hapus Simbol (Regex).js
+++ Hapus Simbol (Regex).js
@@ -1,12 +1,13 @@
 function hapusSimbol(str) {
-  var reg = str.match(/[a-z0-9]+/gi).toString().replace(/,/g, "");;
+    
+    var reg = str.match(/[a-z0-9]+/gi).toString().replace(/,/g, "")
+    
+    return reg
+  }
   
-  return reg;
-}
-
-// TEST CASES
-console.log(hapusSimbol("test%$4aa")); // test4aa
-console.log(hapusSimbol("devel0p3r s3j@@ati")); // devel0p3rs3jati
-console.log(hapusSimbol("ma@#k!an~")); // makan
-console.log(hapusSimbol("coding")); // coding
-console.log(hapusSimbol("1+3-5*2=100")); // 1352100
+  // TEST CASES
+  console.log(hapusSimbol("test%$4aa")); // test4aa
+  console.log(hapusSimbol("devel0p3r s3j@@ati")); // devel0p3rs3jati
+  console.log(hapusSimbol("ma@#k!an~")); // makan
+  console.log(hapusSimbol("coding")); // coding
+  console.log(hapusSimbol("1+3-5*2=100")); // 1352100
